cbd57e269ca4ffac89f1cca23539299917c55ced
Unknown <rivaille.clement@orange.fr>
2017-10-30 13:28:14 +0100

    feat: menu component

---
+++ src/components/HeadMenu.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <li>
+      <router-link to="skills">Skills</router-link>
+    </li>
+    <li>
+      <router-link to="creations">Creations</router-link>
+    </li>
+    <li>
+      <router-link to="experiences">Experiences</router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'head-menu',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+  ul {
+    display: flex;
+    justify-content: center;
+  }
+  li {
+    min-width: 2.5em;
+    margin: 0 0.5em;
+  }
+</style>
--- src/components/TitleHeader.vue
+++ src/components/TitleHeader.vue
@@ -2,15 +2,19 @@
   <header>
     <h1>Clément Rivaille</h1>
     <div>Software Ingineer - Front-End Developer</div>
+    <head-menu></head-menu>
   </header>
 </template>
 
 <script>
+import HeadMenu from './HeadMenu';
+
 export default {
   name: 'title-header',
   data() {
     return {};
   },
+  components: { HeadMenu },
 };
 </script>
 
