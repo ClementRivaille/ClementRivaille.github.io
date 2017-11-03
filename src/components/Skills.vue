e3e682d7579c423a7fc0f92d8c163d109b6c12a0
rivaille-c <rivaille-c@mgdis.fr>
2017-11-03 14:22:16 +0100

    feat: skills and about content

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -27,6 +27,50 @@ export default {
     return {
       languages: ['Javascript', 'HTML5', 'CSS3', 'Java', 'Python', 'C#', 'LaTeX', 'SQL'],
       technologies: ['AngularJS', 'Node.js', 'Web Audio API', 'git', 'Vue.js', 'React', 'Flask'],
+      experiences: [
+        {
+          group: 'MGDIS',
+          location: 'Vannes, France',
+          period: 'nov. 2014 - today',
+          post: 'Software developper',
+          description: 'Web Front-End developent for decision assisting tools used by public organisms',
+        },
+        {
+          group: 'OSYTOS',
+          location: 'Rennes, France',
+          period: 'marsh 2014 - aug. 2014',
+          post: 'Internship',
+          description: 'Conception of a tool facilitating the creation of web pages for dashboard and configuration panels',
+        },
+        {
+          group: 'OSYTOS',
+          location: 'Rennes, France',
+          period: 'june 2013 - aug. 2013',
+          post: 'Internship',
+          description: 'File synchronization between networks, with a web user interface',
+        },
+        {
+          group: 'Groupe Yves-Rocher',
+          location: 'Saint-Avé, France',
+          period: 'april 2011 - june 2011',
+          post: 'Internship',
+          description: 'Regression testing of web-services',
+        },
+      ],
+      formations: [
+        {
+          type: 'Master\'s Degree in Engineering',
+          establishment: 'Université de Rennes 1',
+          formation: 'ESIR, Software engineering',
+          years: '2011 - 2014',
+        },
+        {
+          type: 'Associate\'s degree',
+          establishment: 'Université Bretagne Sud, Vannes',
+          formation: 'IUT, Software development',
+          years: '2009 - 2011',
+        },
+      ],
     };
   },
 };
