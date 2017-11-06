b59727710889b3083fb1e1d70a4eb6e9bea3135c
rivaille-c <rivaille-c@mgdis.fr>
2017-11-06 14:37:36 +0100

    feat(creations): games

--- src/components/Creations.vue
+++ src/components/Creations.vue
@@ -1,13 +1,52 @@
 <template>
-  <h2>Creations</h2>
+  <div>
+    <h2>Creations</h2>
+    
+    <h3>Games</h3>
+    <p>I've started game development in 2016. I'm making short games, mostly for the web. Some have been made during 48 hours jams. You cn find the entire list of them on <a href="https://itooh.itch.io">itch.io</a></p>
+
+    <ul>
+      <li v-for="game in games">
+        <project-link v-bind="game"></project-link>
+      </li>
+    </ul>
+
+  </div>
 </template>
 
 <script>
+import ProjectLink from './ProjectLink';
+
 export default {
   name: 'creations',
   data() {
-    return {};
+    return {
+      games: [
+        {
+          title: 'Moog Memories',
+          url: 'https://itooh.itch.io/moog-memories',
+          description: 'Interactive music in HTML5, made in 48 hours for the Game Music Jam',
+        },
+        {
+          title: 'Tabletop Dance',
+          url: 'https://itooh.itch.io/tabletop-dance',
+          description: 'Puzzle shooter made during the GMTK Jam',
+        },
+        {
+          title: 'Safe Bots',
+          url: 'https://itooh.itch.io/safe-bots',
+          description: 'Puzzle game made in one week',
+        },
+        {
+          title: 'Deep Sea School',
+          url: 'https://itooh.itch.io/deep-sea-school',
+          description: 'Exploration and narration game',
+        },
+      ],
+      experiments: [],
+    };
   },
+  components: { ProjectLink },
 };
 </script>
 
---
+++ src/components/ProjectLink.vue
@@ -0,0 +1,24 @@
+<template>
+  <a :href="url" :title="title">
+    <div>
+      <h4>{{title}}</h4>
+      <img :alt="title" :src="img"/>
+      <p>{{description}}</p>
+    </div>
+    </div>
+  </a>
+</template>
+
+<script>
+
+export default {
+  name: 'project-link',
+  props: [
+    'title',
+    'url',
+    'img',
+    'description',
+  ],
+};
+
+</script>
