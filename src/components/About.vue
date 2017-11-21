00396c742e49e5c8c5ae29d87ee9ba179328ae3e
ClementRivaille <rivaille.clement@orange.fr>
2017-11-21 19:41:10 +0100

    feat(about): add useful informations

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,6 +4,15 @@
   <p>{{ $t('about.dev') }}</p>
   <p>{{ $t('about.games') }}</p>
   <p>{{ $t('about.perso') }}</p>
+
+  <div class="useful">
+    <h3> {{ $t('about.useful.title' ) }} </h3>
+    <ul>
+      <li> <strong>{{ $t('about.useful.location') }}</strong> Vannes, France </li>
+      <li> <strong>{{ $t('about.useful.employer') }}</strong> <a href="https://www.mgdis.fr">MGDIS</a></li>
+      <li> <strong>{{ $t('about.useful.age') }}</strong> {{ age }}</li>
+    </ul>
+  </div>
 </div>
 </template>
 
@@ -11,10 +20,16 @@
 export default {
   name: 'about',
   data() {
-    return {};
+    // Calculate age…
+    const birth = new Date('1991/10/06');
+    const age = Math.floor((new Date() - birth) / 1000 / 60 / 60 / 24 / 365);
+    return { age };
   },
 };
 </script>
 
 <style>
+ul {
+  list-style: none;
+}
 </style>
